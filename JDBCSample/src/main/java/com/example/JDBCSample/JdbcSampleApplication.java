10ee45189d9d1fa467efac45dfb8253bf6bc6fd7
Kimjihun <125746081+zhun0922@users.noreply.github.com>
2023-12-14 18:05:25 +0900

    📁File : JDBCSample

--- JDBCSample/src/main/java/com/example/JDBCSample/JdbcSampleApplication.java
+++ JDBCSample/src/main/java/com/example/JDBCSample/JdbcSampleApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.JDBCSample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JdbcSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(JdbcSampleApplication.class, args);
 	}
 
 }
--- JDBCSample/src/test/java/com/example/JDBCSample/JdbcSampleApplicationTests.java
+++ JDBCSample/src/test/java/com/example/JDBCSample/JdbcSampleApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.JDBCSample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class JdbcSampleApplicationTests {
 
 	@Test
 	void contextLoads() {
